c56c0e9631983ecfce9aa9d3e2d939c0ec5dfdde
Nguyen Van Hung <kk-vanhung.nguyen@vn.panasonic.com>
2023-06-21 10:08:08 +0700

    update

--- main.js
+++ main.js
@@ -1,53 +1,46 @@
-const ytdl = require('ytdl-core');
-const https = require('https'); // or 'https' for https:// URLs
-const fs = require('fs');
+// const ytdl = require('ytdl-core');
+// const https = require('https'); // or 'https' for https:// URLs
+// const fs = require('fs');
 
-ytdl.getInfo('KbIjug1Ijio').then((res) => {
-  console.log(
-    res.formats.filter((format) => format.videoCodec && format.audioCodec)[0]
-  );
-  const dashManifestUrl = res.player_response.streamingData.dashManifestUrl;
-  // console.log(dashManifestUrl);
-  const file = fs.createWriteStream('manifest.mpd');
-  https.get(dashManifestUrl, function (response) {
-    response.pipe(file);
-    file.on('finish', () => {
-      file.close();
-      console.log('Download Completed');
-    });
-  });
-});
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const player = videojs('videoPlayer', {
-//     plugins: {
-//       settingsMenu: {
-//         items: [
-//           'AudioTrackButton',
-//           'PlaybackRateMenuButton',
-//           'RatesButton',
-//         ],
-//         languages: {
-//           settings: 'Settings',
-//           loading: 'Loading',
-//           back: 'Back',
-//           audio: 'Audio',
-//           speed: 'Speed',
-//           quality: 'Quality',
-//         },
-//       },
-//     },
-//     crossOrigin: 'anonymous',
-//     liveui: true,
-//     autoplay: true,
-//     playbackRates: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
-//     nativeControlsForTouch: false,
-//   });
-
-//   player.ready(function () {
-//     player.src({
-//       src: './manifest.mpd',
-//       type: 'application/dash+xml',
+// ytdl.getInfo('KbIjug1Ijio').then((res) => {
+//   console.log(
+//     res.formats.filter((format) => format.videoCodec && format.audioCodec)[0]
+//   );
+//   const dashManifestUrl = res.player_response.streamingData.dashManifestUrl;
+//   // console.log(dashManifestUrl);
+//   const file = fs.createWriteStream('manifest.mpd');
+//   https.get(dashManifestUrl, function (response) {
+//     response.pipe(file);
+//     file.on('finish', () => {
+//       file.close();
+//       console.log('Download Completed');
 //     });
 //   });
 // });
+
+document.addEventListener('DOMContentLoaded', () => {
+  const player = videojs('videoPlayer', {
+    plugins: {
+      settingsMenu: {
+        items: [
+          'AudioTrackButton',
+          'PlaybackRateMenuButton',
+          'RatesButton',
+        ],
+        languages: {
+          settings: 'Settings',
+          loading: 'Loading',
+          back: 'Back',
+          audio: 'Audio',
+          speed: 'Speed',
+          quality: 'Quality',
+        },
+      },
+    },
+    playbackRates: [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2],
+  });
+
+  player.ready(function () {
+    console.log('ready');
+  });
+});
--- plyr.js
+++ plyr.js
@@ -1,23 +1,68 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const source = './manifest.mpd';
-  const dash = dashjs.MediaPlayer().create();
-  const video = document.querySelector('#videoPlayer');
-  dash.initialize(video, source, true);
+  const source =
+    'https://stream2.unica.vn/unica/_definst_/edubit/2839/283848/720.mp4/playlist.m3u8?wowzatokenendTime=1687253128&wowzatokenstartTime=1687242328&wowzatokenhash=FTWmvw9lwWOGebZK-mIH9JOIsxbUgaKd6PraAVHNdbs=&uid=64911397e275cb99968b4582&expired=1687246285&token=18235ea713ad81b66e13c9e1a92a6925d3705686';
+  const video = document.querySelector('video');
 
-  const player = new Plyr(video, {
-    blankVideo: 'https://cdn.plyr.io/static/blank.mp4',
-    autoplay: false,
-    seekTime: 10,
-  });
+  const defaultOptions = {};
 
-  window.player = player;
-  window.dash = dash;
+  if (!Hls.isSupported()) {
+    var player = new Plyr(video, defaultOptions);
+  } else {
+    // For more Hls.js options, see https://github.com/dailymotion/hls.js
+    const hls = new Hls();
+    hls.loadSource(source);
 
-  player.on('ready', (event) => {
-    console.log('ready');
-  });
+    // From the m3u8 playlist, hls parses the manifest and returns
+    // all available video qualities. This is important, in this approach,
+    // we will have one source on the Plyr player.
+    hls.on(Hls.Events.MANIFEST_PARSED, function (event, data) {
+      // Transform available levels into an array of integers (height values).
+      const availableQualities = hls.levels.map((l) => l.height);
+      availableQualities.unshift(0); //prepend 0 to quality array
 
-  player.on('ended', (event) => {
-    console.log('end');
-  });
+      // Add new qualities to option
+      defaultOptions.quality = {
+        default: 0, //Default - AUTO
+        options: availableQualities,
+        forced: true,
+        onChange: (e) => updateQuality(e),
+      };
+      // Add Auto Label
+      defaultOptions.i18n = {
+        qualityLabel: {
+          0: 'Auto',
+        },
+      };
+
+      hls.on(Hls.Events.LEVEL_SWITCHED, function (event, data) {
+        var span = document.querySelector(
+          ".plyr__menu__container [data-plyr='quality'][value='0'] span"
+        );
+        if (hls.autoLevelEnabled) {
+          span.innerHTML = `AUTO (${hls.levels[data.level].height}p)`;
+        } else {
+          span.innerHTML = `AUTO`;
+        }
+      });
+
+      // Initialize new Plyr player with quality options
+      var player = new Plyr(video, defaultOptions);
+    });
+
+    hls.attachMedia(video);
+    window.hls = hls;
+  }
+
+  function updateQuality(newQuality) {
+    if (newQuality === 0) {
+      window.hls.currentLevel = -1; //Enable AUTO quality if option.value = 0
+    } else {
+      window.hls.levels.forEach((level, levelIndex) => {
+        if (level.height === newQuality) {
+          console.log('Found quality match with ' + newQuality);
+          window.hls.currentLevel = levelIndex;
+        }
+      });
+    }
+  }
 });
